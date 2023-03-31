4863abb062c800700f9f337eefaf6cdf80d97dbb
Hum-bee <hsheredia1@gmail.com>
2023-03-31 11:32:02 -0400

    Made changes to DB and dataseed for normalization.

--- databaseQ.sql
+++ databaseQ.sql
@@ -1,52 +1,42 @@
-
 -- SQL queries for creating necessary tables in PostgreSQL.
 
+-- Author
 CREATE TABLE Author (
   authorID SERIAL PRIMARY KEY,
   firstName VARCHAR(255) NOT NULL,
-  lastName VARCHAR(255) NOT NULL,
-  bookID INTEGER,
-  publisherID INTEGER
+  lastName VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE Author_Book (
-  ID SERIAL PRIMARY KEY,
-  authorID INTEGER REFERENCES Author(authorID),
-  bookID INTEGER
-);
-
+-- Genre
 CREATE TABLE Genre (
   genreID SERIAL PRIMARY KEY,
   genreDesc VARCHAR(255) NOT NULL
 );
 
+-- Publisher
 CREATE TABLE Publisher (
   publisherID SERIAL PRIMARY KEY,
   publisherName VARCHAR(255) NOT NULL,
-  publishYear INTEGER,
-  authorID INTEGER REFERENCES Author(authorID),
-  bookID INTEGER
+  publishYear INTEGER
 );
 
+-- Book
 CREATE TABLE Book (
   bookID SERIAL PRIMARY KEY,
   bookTitle VARCHAR(255) NOT NULL,
-  authorID INTEGER REFERENCES Author(authorID),
   publisherID INTEGER REFERENCES Publisher(publisherID),
   genreID INTEGER REFERENCES Genre(genreID),
   numCopies INTEGER NOT NULL
 );
 
-ALTER TABLE Author
-ADD FOREIGN KEY (bookID) REFERENCES Book(bookID),
-ADD FOREIGN KEY (publisherID) REFERENCES Publisher(publisherID);
+-- Author_Book Normalization
+CREATE TABLE Author_Book (
+  ID SERIAL PRIMARY KEY,
+  authorID INTEGER REFERENCES Author(authorID),
+  bookID INTEGER REFERENCES Book(bookID)
+);
 
-ALTER TABLE Author_Book
-ADD FOREIGN KEY (bookID) REFERENCES Book(bookID);
-
-ALTER TABLE Publisher
-ADD FOREIGN KEY (bookID) REFERENCES Book(bookID);
-
+-- Users Supertype
 CREATE TABLE Users (
   UserID SERIAL PRIMARY KEY,
   firstName VARCHAR(255) NOT NULL,
@@ -55,26 +45,31 @@ CREATE TABLE Users (
   password VARCHAR(255) NOT NULL
 );
 
+-- Status
 CREATE TABLE Status (
   statusID SERIAL PRIMARY KEY,
   statusDesc VARCHAR(255) NOT NULL
 );
 
+-- Library Patron Subtype
 CREATE TABLE Library_Patron (
   patronID INTEGER PRIMARY KEY REFERENCES Users(UserID),
   statusID INTEGER REFERENCES Status(statusID)
 );
 
+-- Librarian SUbtype
 CREATE TABLE Librarian (
   librarianID INTEGER PRIMARY KEY REFERENCES Users(UserID)
 );
 
+-- Fine
 CREATE TABLE Fine (
   fineID SERIAL PRIMARY KEY,
   fineAMT FLOAT NOT NULL,
   fineDesc VARCHAR(255) NOT NULL
 );
 
+-- Loan
 CREATE TABLE Loan (
   loanID SERIAL PRIMARY KEY,
   bookID INTEGER REFERENCES Book(bookID),
