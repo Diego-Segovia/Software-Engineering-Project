29279b2031c4904a5f374cb56bd719b3f2ae11d1
spazken <kennedyserrano12@gmail.com>
2023-04-04 11:46:54 -0400

    kenny The Navigation Bar component, used across all pages

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,61 @@
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import './NavBarComponent.css'
+import { Link } from 'react-router-dom';
+
+
+       
+         
+
+
+function NavScrollExample() {
+  return (
+  <div className='nav-scroll'>
+    <Navbar bg="blue" expand="lg" fixed='top' variant='dark'  >
+        
+      <Container fluid className='navbar-Container'>
+        <Navbar.Brand href="#" className='navbar-custom' >Library Management System</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0 "
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Link to="/">Home</Link>
+            <Nav.Link to={'/'}>Profile</Nav.Link>
+            <NavDropdown title="Link" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action5">
+                Something else here
+              </NavDropdown.Item>
+            </NavDropdown>
+            
+            <Link to="/Login">Login</Link>
+            
+          </Nav>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="dark" >Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    </div>
+  );
+}
+
+export default NavScrollExample;
