9e87b17970d04770a93c05257dbc7945357acff0
Diego-Segovia <diego.segovia348@gmail.com>
2023-04-16 23:22:41 -0400

    Add book data prop

--- frontend/src/components/UI/BookSection.jsx
+++ frontend/src/components/UI/BookSection.jsx
@@ -1,11 +1,11 @@
 import Card from "react-bootstrap/Card";
 import BookCarousel from "./BookCarousel";
 
-function BookSection() {
+function BookSection(props) {
   return (
     <Card className="m-2 p-0 shadow">
       <Card.Body>
-        <BookCarousel />
+        <BookCarousel bookData={props.bookData} />
       </Card.Body>
     </Card>
   );
