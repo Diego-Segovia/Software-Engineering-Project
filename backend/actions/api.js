368a2598d96ac71fb6089a0de4b5c680c167cb33
Diego-Segovia <diego.segovia348@gmail.com>
2023-04-22 17:33:02 -0400

    Add genre & publisher information for book

--- backend/actions/api.js
+++ backend/actions/api.js
@@ -2,13 +2,18 @@ const db = require("../models");
 
 const getBooks = async (req, res) => {
   const data = await db.Book.findAll({
+    attributes: ["isbn", "booktitle", "numcopies", "bookimage", "synopsis"],
     include: [
       {
         model: db.Author,
-        through: { attributes: [] }, // Hide the intermediate table attributes in the output
+        attributes: ["firstname", "lastname"],
+        through: { attributes: [] }, // Exclude the join table (AuthorBook) attributes
       },
+      { model: db.Genre, attributes: ["genredesc"] },
+      { model: db.Publisher, attributes: ["publishername", "publishyear"] },
     ],
   });
+
   const booksJSON = data.map((book) => book.toJSON());
   res.json(booksJSON);
   console.log("Books sent!");
