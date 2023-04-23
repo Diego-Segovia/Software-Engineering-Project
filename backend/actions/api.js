d0d3d9e3d35b5400e564cb9e77e058e8e2ee2e40
Diego-Segovia <diego.segovia348@gmail.com>
2023-04-22 21:38:59 -0400

    Add function to get book based on id from database

--- backend/actions/api.js
+++ backend/actions/api.js
@@ -1,24 +1,50 @@
 const db = require("../models");
 
-const getBooks = async (req, res) => {
-  const data = await db.Book.findAll({
-    attributes: ["isbn", "booktitle", "numcopies", "bookimage", "synopsis"],
-    include: [
-      {
-        model: db.Author,
-        attributes: ["firstname", "lastname"],
-        through: { attributes: [] }, // Exclude the join table (AuthorBook) attributes
-      },
-      { model: db.Genre, attributes: ["genredesc"] },
-      { model: db.Publisher, attributes: ["publishername", "publishyear"] },
-    ],
-  });
+const bookInfo = {
+  attributes: [
+    "bookid",
+    "isbn",
+    "booktitle",
+    "numcopies",
+    "bookimage",
+    "synopsis",
+  ],
+  include: [
+    {
+      model: db.Author,
+      attributes: ["firstname", "lastname"],
+      through: { attributes: [] }, // Exclude the external table attributes
+    },
+    { model: db.Genre, attributes: ["genredesc"] },
+    { model: db.Publisher, attributes: ["publishername", "publishyear"] },
+  ],
+};
 
+const getBooks = async (req, res) => {
+  const data = await db.Book.findAll(bookInfo);
   const booksJSON = data.map((book) => book.toJSON());
   res.json(booksJSON);
   console.log("Books sent!");
 };
 
+const getBookById = async (req, res) => {
+  const bookId = req.params.bookId;
+
+  const book = await db.Book.findOne({
+    where: { bookid: bookId },
+    ...bookInfo,
+  });
+
+  if (book) {
+    res.json(book.toJSON());
+    console.log("A book was found.");
+  } else {
+    res.status(404).send(`No book found with id: ${bookId}`);
+    console.log(`No book found with bookId: ${bookId}`);
+  }
+};
+
 module.exports = {
   getBooks,
+  getBookById,
 };
