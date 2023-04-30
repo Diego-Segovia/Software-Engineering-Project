8bf1c880a0f4231e6e0270b60a729995c3c7e7c5
Diego-Segovia <diego.segovia348@gmail.com>
2023-04-29 21:15:29 -0400

    Add loan creation in the database

--- backend/actions/api.js
+++ backend/actions/api.js
@@ -1,6 +1,6 @@
 const db = require("../models");
 
-const bookInfo = {
+const generalBookInfoQuery = {
   attributes: [
     "bookid",
     "isbn",
@@ -20,31 +20,46 @@ const bookInfo = {
   ],
 };
 
+// Retrieve all books from database
 const getBooks = async (req, res) => {
-  const data = await db.Book.findAll(bookInfo);
+  const data = await db.Book.findAll(generalBookInfoQuery);
   const booksJSON = data.map((book) => book.toJSON());
   res.json(booksJSON);
   console.log("Books sent!");
 };
 
+// Retrieve a specific book based on ID
 const getBookById = async (req, res) => {
   const bookId = req.params.bookId;
 
   const book = await db.Book.findOne({
     where: { bookid: bookId },
-    ...bookInfo,
+    ...generalBookInfoQuery,
   });
 
   if (book) {
     res.json(book.toJSON());
-    console.log("A book was found.");
+    console.log(`Book ${bookId} was found.`);
   } else {
     res.status(404).send(`No book found with id: ${bookId}`);
     console.log(`No book found with bookId: ${bookId}`);
   }
 };
 
+// Create a book loan in database
+const createLoan = async (req, res) => {
+  try {
+    const loan = await db.Loan.create(req.body);
+    console.log("Loan created successfully.");
+    res.status(201).json(loan);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+    console.log("Loan creation error!");
+  }
+};
+
 module.exports = {
   getBooks,
   getBookById,
+  createLoan,
 };
