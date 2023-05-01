c3fcf2f1e5385a140019ce579cca9a71158143fb
Diego-Segovia <diego.segovia348@gmail.com>
2023-05-01 18:40:07 -0400

    Handle librarian & patron login

--- frontend/src/components/LoginModal.jsx
+++ frontend/src/components/LoginModal.jsx
@@ -1,21 +1,74 @@
-import React, { useState } from "react";
+import { useState, useContext, useRef } from "react";
 import { Button, Modal, Form, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSignIn } from "@fortawesome/free-solid-svg-icons";
+import { AuthContext } from "../utils/auth-context";
+import { loginUser } from "../utils/utils";
 
 const LoginModal = () => {
+  const auth = useContext(AuthContext);
   const [show, setShow] = useState(false);
-  const [isLibrarian, setisLibrarian] = useState(false);
+
+  const usernameRef = useRef();
+  const passwordRef = useRef();
+  const staffIdRef = useRef();
 
   const handleClose = () => setShow(false);
-  const handleShow = () => {
-    setisLibrarian(false);
-    setShow(true);
+  const handleShow = () => setShow(true);
+
+  const handleLibrarianLogin = () => auth.setAsLibrarian(true);
+  const handlePatronLogin = () => auth.setAsLibrarian(false);
+
+  const onPatronLogin = async (e) => {
+    e.preventDefault();
+    const data = {
+      username: usernameRef.current.value,
+      password: passwordRef.current.value,
+    };
+
+    try {
+      const responseData = await loginUser(data);
+
+      if (responseData.userrole == "Librarian") {
+        alert("only for patron login");
+        throw new Error("Login failed");
+      }
+
+      auth.setUser(responseData);
+      auth.setAsLibrarian(false);
+      auth.login();
+
+      handleClose();
+    } catch (error) {
+      console.error("Login error:", error);
+    }
   };
 
-  const handleLibrarianLogin = () => setisLibrarian(true);
-  const handlePatronLogin = () => setisLibrarian(false);
+  const onLibrarianLogin = async (e) => {
+    e.preventDefault();
+    const data = {
+      username: usernameRef.current.value,
+      password: passwordRef.current.value,
+    };
+
+    try {
+      const responseData = await loginUser(data);
+
+      if (responseData.userrole == "Library_Patron") {
+        alert("only for librarian login");
+        throw new Error("Login failed");
+      }
+
+      auth.setUser(responseData);
+      auth.setAsLibrarian(true);
+      auth.login();
+
+      handleClose();
+    } catch (error) {
+      console.error("Login error:", error);
+    }
+  };
 
   return (
     <>
@@ -29,26 +82,42 @@ const LoginModal = () => {
 
       <Modal show={show} onHide={handleClose} centered>
         <Modal.Header closeButton>
-          <Modal.Title>Login</Modal.Title>
+          {auth.isLibrarian ? (
+            <Modal.Title>Librarian Login</Modal.Title>
+          ) : (
+            <Modal.Title> Patron Login</Modal.Title>
+          )}
         </Modal.Header>
         <Modal.Body>
           <Form>
-            {isLibrarian && (
+            {auth.isLibrarian && (
               <Form.Group controlId="staffId" className="mb-1">
                 <Form.Label>Staff ID</Form.Label>
-                <Form.Control type="text" placeholder="Enter staff id" />
+                <Form.Control
+                  ref={staffIdRef}
+                  type="text"
+                  placeholder="Enter staff id"
+                />
               </Form.Group>
             )}
             <Form.Group controlId="username" className="mb-1">
               <Form.Label>Username</Form.Label>
-              <Form.Control type="text" placeholder="Enter username" />
+              <Form.Control
+                ref={usernameRef}
+                type="text"
+                placeholder="Enter username"
+              />
             </Form.Group>
 
             <Form.Group controlId="password" className="mb-1">
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
+              <Form.Control
+                ref={passwordRef}
+                type="password"
+                placeholder="Password"
+              />
             </Form.Group>
-            {!isLibrarian && (
+            {!auth.isLibrarian && (
               <Form.Group controlId="signup">
                 <Form.Text className="text-muted">
                   Don't have an account?{" "}
@@ -64,7 +133,6 @@ const LoginModal = () => {
                 <Form.Text className="text-muted">
                   Login as librarian?{" "}
                   <Link
-                    to="/patron-signup"
                     onClick={handleLibrarianLogin}
                     style={{ textDecoration: "none" }}
                   >
@@ -73,7 +141,7 @@ const LoginModal = () => {
                 </Form.Text>
               </Form.Group>
             )}
-            {isLibrarian && (
+            {auth.isLibrarian && (
               <Form.Group controlId="signup">
                 <Form.Text className="text-muted">
                   Don't have an account?{" "}
@@ -95,24 +163,26 @@ const LoginModal = () => {
                     Login
                   </Link>
                 </Form.Text>
-                <br />
-                <Form.Text>
-                  <Link
-                    to="/librarianDashboard"
-                    style={{ textDecoration: "none" }}
-                  >
-                    Librarian Dashboard
-                  </Link>
-                </Form.Text>
               </Form.Group>
             )}
           </Form>
         </Modal.Body>
         <Modal.Footer>
           <Col className="d-flex justify-content-end">
-            <Button variant="primary" onClick={handleClose}>
-              Login
-            </Button>
+            {!auth.isLibrarian && (
+              <Button variant="primary" type="submit" onClick={onPatronLogin}>
+                Login
+              </Button>
+            )}
+            {auth.isLibrarian && (
+              <Button
+                variant="primary"
+                type="submit"
+                onClick={onLibrarianLogin}
+              >
+                Login
+              </Button>
+            )}
           </Col>
         </Modal.Footer>
       </Modal>
