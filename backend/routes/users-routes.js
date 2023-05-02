6af7086e333a55ee24e79c3f8ffc5d217a2470ab
Hum-bee <hsheredia1@gmail.com>
2023-05-02 00:55:35 -0400

    Add updateUserData route.

--- backend/routes/users-routes.js
+++ backend/routes/users-routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { signUpUser, loginUser, getUsers, getUserById } = require("../actions");
+const { signUpUser, loginUser, getUsers, getUserById, updateUserData } = require("../actions");
 
 router.get("/:userId", getUserById);
 
@@ -9,5 +9,7 @@ router.get("/", getUsers);
 router.post("/signup", signUpUser);
 
 router.post("/login", loginUser);
+
+router.put("/updateUser", updateUserData);
 
 module.exports = router;
