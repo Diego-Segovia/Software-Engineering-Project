6cfdc8efb8e4ebebfb831d21460945dc7c638d9b
Diego-Segovia <diego.segovia348@gmail.com>
2023-05-02 22:25:47 -0400

    Add approve and deny buttons

--- frontend/src/components/TableComponent.jsx
+++ frontend/src/components/TableComponent.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 
-function TableComponent({ data }) {
+function TableComponent({ data, isRequest }) {
   const getHeadings = () => {
     return Object.keys(data[0]);
   };
 
   return (
-    <div className="TableComponent">
+    <div className="TableComponent mx-4 mt-3 mb-4 card">
       <table class="table table-striped">
         <thead>
           <tr>
             {getHeadings().map((heading) => {
               return <th key={heading}>{heading}</th>;
             })}
+            {isRequest && <th>Transaction</th>}
           </tr>
         </thead>
         <tbody>
@@ -22,6 +23,16 @@ function TableComponent({ data }) {
                 {getHeadings().map((key, index) => {
                   return <td key={row[key]}>{row[key]}</td>;
                 })}
+                {isRequest && (
+                  <td>
+                    <button type="button" class="btn btn-success btn-sm me-3">
+                      Approve
+                    </button>
+                    <button type="button" class="btn btn-danger btn-sm">
+                      Deny
+                    </button>
+                  </td>
+                )}
               </tr>
             );
           })}
