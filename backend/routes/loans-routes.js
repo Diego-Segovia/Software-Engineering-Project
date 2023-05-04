92b250f5270eecb1cf3c66cfa1968746a7d7a014
Diego-Segovia <diego.segovia348@gmail.com>
2023-05-03 23:53:27 -0400

    Add routes to delete loan & to approve loan

--- backend/routes/loans-routes.js
+++ backend/routes/loans-routes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { createLoan, getLoans } = require("../actions");
+const { createLoan, getLoans, deleteLoan, approveLoan } = require("../actions");
 
 router.post("/", createLoan);
 router.get("/", getLoans);
-
+router.delete("/delete/:loanId", deleteLoan);
+router.patch("/loanOut/:loanId", approveLoan);
 module.exports = router;
