1a65acfc521cae0d3631403a1e1a31eb657551a2
Hum-bee <hsheredia1@gmail.com>
2023-05-04 02:50:20 -0400

    Create API endpoint for loans with fine data

--- backend/actions/api.js
+++ backend/actions/api.js
@@ -242,6 +242,44 @@ const approveLoan = async (req, res, next) => {
   }
 };
 
+const loanWithFineQuery = {
+  attributes: [
+    "loanid",
+    "patronid",
+    "fineid"
+  ],
+  include: [
+    {
+      model: db.Fine,
+      attributes: ["fineamt"],
+    }
+  ]
+}
+
+// Retrieve loans with fine information for a specific patron from the database
+const getLoansWithFinesByPatronId = async (req, res, next) => {
+const patronId = req.params.patronId;
+try {
+  const data = await db.Loan.findAll({
+    where: { patronid: patronId },
+    ...loanWithFineQuery,
+  });
+
+  if (data.length === 0) {
+    res.status(404).json({ message: `No fines found for patron ID ${patronId}.` });
+    console.log(`No fines found for patron ID ${patronId}.`);
+  } else {
+    console.log("Received user data:", data)
+    const loansJSON = data.map((loan) => loan.toJSON());
+    res.status(200).json(loansJSON);
+    console.log(`Loans with fines for patron ID ${patronId} sent!`);
+  }
+} catch (error) {
+  res.status(500).json({ message: "An error occurred while fetching the data." });
+  console.error(`Error fetching loans with fines for patron ID ${patronId}: ${error}`);
+}
+};
+
 module.exports = {
   getBooks,
   getBookById,
@@ -254,4 +292,5 @@ module.exports = {
   updateUserData,
   deleteLoan,
   approveLoan,
+  getLoansWithFinesByPatronId,
 };
