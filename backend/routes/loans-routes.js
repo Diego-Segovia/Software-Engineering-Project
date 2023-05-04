2e735e173138c6b8249f1f8f9484cc3010e4d859
Hum-bee <hsheredia1@gmail.com>
2023-05-04 00:40:13 -0400

    Create route to get loans with fine

--- backend/routes/loans-routes.js
+++ backend/routes/loans-routes.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const { createLoan } = require("../actions");
+const { createLoan, getLoansWithFinesByPatronId } = require("../actions");
 
 router.post("/", createLoan);
+router.get("/:patronId/fines", getLoansWithFinesByPatronId);
 
 module.exports = router;
